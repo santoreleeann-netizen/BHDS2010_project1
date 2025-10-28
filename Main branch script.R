308f0550f2a6f549de89f02e79581baa4acb1115
Lee Ann <santore.leeann@gmail.com>
2025-10-28 14:30:52 -0400

    Here is the script for the main branch that recreates the dataset and correctly assigns names and classes to each variable. Now we can start adding branches and completing each part on our own time using this as the basis.

---
+++ Main branch script.R
@@ -0,0 +1,45 @@
+#Libraries
+library(readr)
+
+#First I will import the dataset we will be working from and call it raw
+#raw <- read_csv("C:/Users/Owner/Downloads/TextMessages.csv")
+
+#Then I will recreate that data here so that my partner and I can work on it
+#without having to worry about it being located in different spots on each
+#of our computers. This function will output the code needed to recreate the raw
+#data frame
+#dput(as.data.frame(raw))
+
+#Then I can take that code and turn it into the df here
+df <- structure(list(Group = c(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+                               1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
+                               2,2), Baseline = c(52,68,85,47,73,57,63,50,66,
+                              60,51,72,77,57,79,75,53,72,62,71,53,64,79,75
+                              ,60,65,57,66,71,75,61,80,66,53,62,61,77,66,52
+                              ,60,58,54,72,71,87,75,57,59,46,89), Six_months
+                     = c(32,48,62,16,63,53,59,58,59,57,60,56,61,52,9,76,38,63,53
+                         ,61,50,78,33,68,59,62,50,62,61,70,64,64,55,47,61,56,64,
+                         62,47,56,78,74,61,61,78,62,71,55,46,79), Participant = 
+                       c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,
+                         22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,
+                         40,41,42,43,44,45,46,47,48,49,50)), 
+                row.names = c(NA,-50L), class = "data.frame")
+
+#Now that we both have the df available, I will make sure all of our
+#variables are named appropriately and are of the correct classes
+names(df)
+#These names are of reasonable lengths and descriptions so we can keep them as
+#is
+class (df$Group)
+class (df$Baseline)
+class (df$Six_months)
+class (df$Participant)
+#Currently all of our variables are numeric. This is appropriate for Baseline,
+#Six_months, and Participants variables. Group however should be a factor.
+#We will do that here
+df$Group <- as.factor(df$Group)
+#And check that it worked here
+is.factor(df$Group)
+#It is TRUE confirming we successfully converted it to a factor
+#Now that our data is imported and has the correct names and classes, we can
+#begin to create our figures and start our analysis. 
